80e5f09600582ab760c144d81189db6b517003df
Amiram Yassif <amiram.yassif@live.biu.ac.il>
2021-11-18 18:27:59 +0200

    aaa

---
+++ timeseries.h
@@ -0,0 +1,20 @@
+
+
+#ifndef TIMESERIES_H_
+#define TIMESERIES_H_
+
+using namespace std;
+
+class TimeSeries{
+
+public:
+
+	TimeSeries(const char* CSVfileName){
+		cout << "DIE";
+	}
+
+};
+
+
+
+#endif /* TIMESERIES_H_ */
